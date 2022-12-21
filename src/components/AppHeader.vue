23143abee41c35dba247c77d9da25e01d3a112d1
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-21 11:16:03 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "Appheader",
+}
+</script>
+
+<template>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables.scss' as *;
+@use '../style/general.scss' as *;
+</style>
